b822411d80f7337cab5d243b9b216195a0fba91c
Gzp_ <741424975@qq.com>
2016-05-26 22:51:08 +0800

    make some change - admin.py

--- MyApp/admin.py
+++ MyApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from MyApp.models import *
 
-# Register your models here.
+admin.site.register(Bundle)
+admin.site.register(Comment)
+admin.site.register(CommentLocation)
+admin.site.register(Scan)
+admin.site.register(ScanLocation)
