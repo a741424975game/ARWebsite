42f4c9d537b2958a3a71c9f5cf14dcdafe7956a1
Gzp_ <741424975@qq.com>
2016-05-27 21:45:16 +0800

    add upload model
    增加上传功能

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -2,21 +2,28 @@
 from django import forms
 from MyApp.models import *
 
+
 # register form 注册表单
 class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control", "id": "userName", "type": "text", "placeholder": "User Name",
+               "required": "required",}),
+                               min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
 
-    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control","id":"userName","type":"text","placeholder": "User Name", "required": "required",}),
-                                min_length=3,max_length=30, error_messages={"required": "username 不能为空",})
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={"class": "form-control", "id": "email", "placeholder": "Email", "type": "email",
+               "required": "required",}),
+                             max_length=50, error_messages={"required": "email 不能为空",})
 
-    email = forms.EmailField(widget=forms.TextInput(attrs={"class":"form-control","id":"email","placeholder": "Email", "type":"email","required": "required",}),
-                                max_length=50, error_messages={"required": "email 不能为空",})
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"class": "form-control", "id": "Password1", "placeholder": "Password", "type": "password",
+               "required": "required",}),
+                               min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","id":"Password1","placeholder": "Password", "type":"password","required": "required",}),
-                                min_length=8,max_length=30, error_messages={"required": "password 不能为空",})
-
-
-    rePassword = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","id":"Password2","placeholder": "Password", "type":"password","required": "required",}),
-                                min_length=8,max_length=30, error_messages={"required": "password 不能为空",})
+    rePassword = forms.CharField(widget=forms.PasswordInput(
+        attrs={"class": "form-control", "id": "Password2", "placeholder": "Password", "type": "password",
+               "required": "required",}),
+                                 min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
     def clean_rePassword(self):
         password = self.cleaned_data.get('password')
@@ -31,22 +38,29 @@ class RegisterForm(forms.Form):
             raise forms.ValidationError("用户名已存在")
         return username
 
+
 # login form 登录表单
 class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control", "id": "userName", "type": "text", "placeholder": "User Name",
+               "required": "required",}),
+                               min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
 
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "id": "userName", "type": "text", "placeholder": "User Name","required": "required",}),
-                            min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"class": "form-control", "id": "Password1", "placeholder": "Password", "type": "password",
+               "required": "required",}),
+                               min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control", "id": "Password1", "placeholder": "Password", "type": "password","required": "required",}),
-                            min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
 # upload form 上传表单
 class UploadForm(forms.Form):
+    modelName = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Model Name",
+               "required": "required",}),
+                                max_length=30, error_messages={"required": "modelName 不能为空",})
 
-    modelName = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Model Name","required": "required",}),
-                            max_length=30, error_messages={"required": "modelName 不能为空",})
-
-    note = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Model Name",}))
+    note = forms.CharField(widget=forms.Textarea(
+        attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Model Name",}))
 
     model = forms.FileField()
 
--- MyApp/views.py
+++ MyApp/views.py
@@ -5,14 +5,16 @@ from httplib import HTTPResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.hashers import make_password
-from models import  *
+from models import *
 from forms import *
 
 # 输出日志信息
 logger = logging.getLogger('MyApp.views')
 
+
 def index(request):
-    return render(request,'index.html',locals())
+    return render(request, 'index.html', locals())
+
 
 # 注册
 def doRegister(request):
@@ -29,17 +31,15 @@ def doRegister(request):
                 user.save()
 
                 # 登录
-                user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+                user.backend = 'django.contrib.auth.backends.ModelBackend'  # 指定默认的登录验证方式
                 login(request, user)
                 return redirect('index')
-
-            else:
-                return render(request, 'register.html',locals())
         else:
             registerForm = RegisterForm()
     except Exception as e:
         logger.error(e)
     return render(request, 'register.html', locals())
+
 
 # 登陆
 def doLogin(request):
@@ -52,25 +52,40 @@ def doLogin(request):
                 password = loginForm.cleaned_data["password"]
                 user = authenticate(username=username, password=password)
                 if user is not None:
-                    user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+                    user.backend = 'django.contrib.auth.backends.ModelBackend'  # 指定默认的登录验证方式
                     login(request, user)
                 else:
                     return render(request, 'login.html', {
-                        "loginForm":loginForm,
-                        "loginFailed":"账户或密码错误",
+                        "loginForm": loginForm,
+                        "loginFailed": "账户或密码错误",
                     })
                 return redirect('index')
-            else:
-                return render(request, 'login.html', locals())
         else:
             loginForm = LoginForm()
     except Exception as e:
         logger.error(e)
     return render(request, 'login.html', locals())
 
+
 def addModel(request):
-    uploadForm = UploadForm()
-    return render(request,'add-model.html',locals())
+    try:
+        if request.method == 'POST':
+            uploadForm = UploadForm(request.POST, request.FILES)
+            if uploadForm.is_valid():
+                bundle = Bundle.objects.create(id_user=request.user if request.user.is_authenticated() else None,
+                                               name=uploadForm.cleaned_data["modelName"],
+                                               # config_info=,
+                                               # QRcode=,
+                                               model=uploadForm.cleaned_data["model"],
+                                               imageTarget=uploadForm.cleaned_data["imageTarget"],
+                                               note=uploadForm.cleaned_data["note"],
+                                               )
+                bundle.save()
+        else:
+            uploadForm = UploadForm()
+    except Exception as e:
+        logger.error(e)
+    return render(request, 'add-model.html', locals())
 
 def model(request):
-    return render(request,'models.html',locals())
+    return render(request, 'models.html', locals())
