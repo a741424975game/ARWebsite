fa2e89bd1faf36e8aecc7c393394e38a57cbd05d
Gzp_ <741424975@qq.com>
2016-07-15 12:55:05 +0800

    add detection
    可以检测用户是否使用我们的app扫描

--- MyApp/views.py
+++ MyApp/views.py
@@ -246,26 +246,30 @@ def ar_config_info_api(request):
     try:
         bundle_id = request.GET.get('bundle_id')
         ip = request.META['REMOTE_ADDR']
-        if ip is not None and bundle_id is not None and Bundle.objects.filter(id=bundle_id):
-            url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip
-            response = requests.get(url)
-            data = location_handle(response.json())
-            if not Locations.objects.filter(province=data['data']['region'], city=data['data']['city'],
-                                            county=data['data']['county'], ):
-                location = Locations.objects.create(province=data['data']['region'],
-                                                    city=data['data']['city'],
-                                                    county=data['data']['county'],
-                                                    )
-                location.save()
-            else:
-                location = Locations.objects.get(province=data['data']['region'],
-                                                 city=data['data']['city'],
-                                                 county=data['data']['county'],
-                                                 )
-            bundle = Bundle.objects.get(id=bundle_id)
-            Scan.objects.create(id_bundle=bundle, id_location=location)
-            config_info = bundle.config_info
-            return HttpResponse(config_info)
+        is_ar_scanner = request.GET.get('is_ar_scanner')
+        if is_ar_scanner is not None:
+            if ip is not None and bundle_id is not None and Bundle.objects.filter(id=bundle_id):
+                url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip
+                response = requests.get(url)
+                data = location_handle(response.json())
+                if not Locations.objects.filter(province=data['data']['region'], city=data['data']['city'],
+                                                county=data['data']['county'], ):
+                    location = Locations.objects.create(province=data['data']['region'],
+                                                        city=data['data']['city'],
+                                                        county=data['data']['county'],
+                                                        )
+                    location.save()
+                else:
+                    location = Locations.objects.get(province=data['data']['region'],
+                                                     city=data['data']['city'],
+                                                     county=data['data']['county'],
+                                                     )
+                bundle = Bundle.objects.get(id=bundle_id)
+                Scan.objects.create(id_bundle=bundle, id_location=location)
+                config_info = bundle.config_info
+                return HttpResponse(config_info)
+        else:
+            return HttpResponse('Download')
     except Exception as e:
         logger.error(e)
     return HttpResponse('error')
