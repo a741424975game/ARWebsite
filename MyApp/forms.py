290f1ed2bf2322e5a4fded51f56a5d1efe810a77
Gzp_ <741424975@qq.com>
2016-07-25 20:13:00 +0800

    add forms
    添加商品链接跟识别图

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -8,22 +8,22 @@ class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
         attrs={"class": "form-control", "id": "userName", "type": "text", "placeholder": "User Name",
                "required": "required",}),
-                               min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
+        min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
 
     email = forms.EmailField(widget=forms.TextInput(
         attrs={"class": "form-control", "id": "email", "placeholder": "Email", "type": "email",
                "required": "required",}),
-                             max_length=50, error_messages={"required": "email 不能为空",})
+        max_length=50, error_messages={"required": "email 不能为空",})
 
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={"class": "form-control", "id": "Password1", "placeholder": "Password", "type": "password",
                "required": "required",}),
-                               min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
+        min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
     rePassword = forms.CharField(widget=forms.PasswordInput(
         attrs={"class": "form-control", "id": "Password2", "placeholder": "Password", "type": "password",
                "required": "required",}),
-                                 min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
+        min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
     def clean_rePassword(self):
         password = self.cleaned_data.get('password')
@@ -44,24 +44,28 @@ class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
         attrs={"class": "form-control", "id": "userName", "type": "text", "placeholder": "User Name",
                "required": "required",}),
-                               min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
+        min_length=3, max_length=30, error_messages={"required": "username 不能为空",})
 
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={"class": "form-control", "id": "Password1", "placeholder": "Password", "type": "password",
                "required": "required",}),
-                               min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
+        min_length=8, max_length=30, error_messages={"required": "password 不能为空",})
 
 
 # upload form 上传表单
 class UploadForm(forms.Form):
     modelName = forms.CharField(widget=forms.TextInput(
-        attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Model Name",
+        attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "名称",
                "required": "required",}),
-                                max_length=30, error_messages={"required": "modelName 不能为空",})
+        max_length=30, error_messages={"required": "modelName 不能为空",})
 
     note = forms.CharField(widget=forms.Textarea(
-        attrs={"class": "form-control", "id": "modelName", "type": "text", "placeholder": "Descriptions", }))
+        attrs={"class": "form-control", "id": "modelNote", "type": "text", "placeholder": "描述",}))
+
+    product_link = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={"class": "form-control", "id": "productLink", "type": "text", "placeholder": "商品链接(若无可以留空)",}
+    ))
 
     model = forms.FileField()
 
-    # imageTarget = forms.ImageField()
+    imageTarget = forms.ImageField(required=False)
